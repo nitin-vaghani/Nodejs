14b38b17c7e9d10142a703758cf6b805f6e2d3a1
Nitin Vaghani <nitinvaghani34@gmail.com>
2022-08-02 11:21:12 +0530

    Create pm2.sh

---
+++ pm2.sh
@@ -0,0 +1,9 @@
+PM2 Start Engage.js --name Warp1
+PM2 Start Engage.js --name Warp2
+
+
+PORT=5003 pm2 start routes.js --name app5003
+PORT=5002 pm2 start routes.js --name app5002
+PORT=5004 pm2 start routes.js --name app5004
+
+https://getstream.io/blog/running-pm2-node-js-in-production-environments/
